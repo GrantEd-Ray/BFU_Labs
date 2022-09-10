d6bcf2b6f8a4067dff0b3a9c8d2200ca07da9b4e
GrantEd-Ray <71932889+GrantEd-Ray@users.noreply.github.com>
2022-09-10 11:25:09 +0200

    Add files via upload

---
+++ Lab.cpp
@@ -0,0 +1,54 @@
+﻿/*#include <iostream>
+
+int main()
+{
+    int a;
+    int b;
+    int c;
+    std::cin >> a >> b >> c;
+
+    if (((a + b) % c == 0) and (c % b == 0))
+    {
+        int s = (a + b) / c - c / b;
+        std::cout << s;
+    }
+    else if (((a + b) % c == 0) and (c % b > 0))
+    {
+        int s = (a + b) / c + b * c;
+        std::cout << s;
+    }
+    else
+    {
+        int s = a - b + c;
+        std::cout << s;
+    }
+}*/
+
+#include <iostream>
+
+int main()
+{
+    int n;
+    std::cin >> n;
+
+    switch (n)
+    {
+        case 1:
+            std::cout << "Apple";
+            break;
+        case 2:
+            std::cout << "Mango";
+            break;
+        case 3:
+            std::cout << "Milk";
+            break;
+        case 4:
+            std::cout << "Juice";
+            break;
+        case 5:
+            std::cout << "Cornflakes";
+            break;
+        default: 
+            std::cout << "No product";
+    }
+}
