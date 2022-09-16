61a3cd7616ffa445543d84e359c68a59d7ad3301
GrantEd-Ray <71932889+GrantEd-Ray@users.noreply.github.com>
2022-09-16 16:01:01 +0200

    Lab 1

---
+++ Lab 1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main()
+{
+    float n;
+    float x;
+    
+    std::cout << "Enter root: ";
+    std::cin >> n;
+
+    std::cout << "Enter number: ";
+    std::cin >> x;
+
+    float a = pow(x, 1 / n);
+    std::cout << "" << a;
+}
