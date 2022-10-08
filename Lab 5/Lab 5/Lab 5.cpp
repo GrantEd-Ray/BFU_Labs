b3cfa0eb63bfb1ade62a7c857c1faf8a1f320562
GrantEd-Ray <71932889+GrantEd-Ray@users.noreply.github.com>
2022-10-08 11:39:18 +0200

    Lab5.2

---
+++ Lab 5/Lab 5/Lab 5.cpp
@@ -0,0 +1,43 @@
+﻿// #2
+
+#include <iostream>
+#include <fstream>
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    char str[100];
+    std::ifstream in("Input.txt", std::ios::in);
+    if (!in.is_open())
+    {
+        std::cout << "Error!";
+        return -1;
+    }
+    //in.getline(str, 100);
+    //std::cout << str;
+
+    char words[100][100];
+    int size = 0;
+
+    while (!in.eof())
+    {
+        in >> str;
+        strcpy_s(words[size], str);
+        size++;
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < strlen(words[i]); j++)
+        {
+            if (127 < words[i][j] < 160)
+                words[i][j] += 32;
+        }
+        std::cout << words[i] << std::endl;
+    }
+    //char tmp[100] = "";
+    //for (int i = 0; i < strlen(str); i++) {
+        //char letter[100] = { str[i] };
+        //strcat_s(tmp, letter);
+    //}
+}
